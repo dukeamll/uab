e568d17a89cfc3e9dba1f90192316b573891f77c
bohaohuang <bohao.huang@duke.edu>
2017-12-12 14:16:10 -0500

    cross val maker functions & example script in ipynb

--- uabCrossValMaker.py
+++ uabCrossValMaker.py
@@ -5,16 +5,67 @@ Created on Mon Dec 11 15:01:14 2017
 
 @author: jordan
 
-Classes defined to split the dataset for training, validation, and testing.  The folds are one number per chip or tile (depending on the input)
+Classes defined to split the dataset for training, validation, and testing.
+The folds are one number per chip or tile (depending on the input)
+
+The assumption of file names:
+    0. Each component (e.g., city name, tile number, patch id, etc) can be concatenated via underscore ('_') or nothing
+       if the two components belong to different category (alphabets and digits)
+    1. city name comes first, can include alphabetic characters and dash('-')
+    2. The tile number follows city name
+    3. The patch id follows file names
+
+The file names in fileList can be only the file name of path to the file name, the functions will handle this
 """
 
 import os
+import re
 
 def uabUtilGetFolds(parentDir, fileList, xvalType):
-    if(xvalType == 0):
+    if('city' in xvalType):
         xvalObj = uabXvalByCity()
+    elif('tile' in xvalType):
+        xvalObj = uabXvalByTile()
+    else:
+        xvalObj = uabXvalParent()
     
     return xvalObj.getFolds(parentDir, fileList)
+
+
+def concat_list(name_list):
+    """
+    concatenate a list to string if is a list
+    :param name_list: list of names
+    :return: a string
+    """
+    if not isinstance(name_list, str):
+        s = ''
+        for name in name_list:
+            s += name.split('/')[-1]
+    else:
+        s = name_list.split('/')[-1]
+    return s
+
+
+def getCityName(file_name):
+    """
+    Get city name of a string, it is assumed that city name is at the beginning of the string and only contain alphas and
+    '-'
+    :param file_name: file name string, all strings will be concatenated if it is a list
+    :return: city name in string
+    """
+    s = concat_list(file_name)
+    return re.findall('^[a-zA-Z\-]*', s)[0]
+
+
+def getTileNumber(file_name):
+    """
+    Get tile number of a string, it is assumed that tile number is following the city name and only contains digits
+    :param file_name: file name string, all strings will be concatenated if it is a list
+    :return: tile name as integer
+    """
+    s = concat_list(file_name)
+    return int(re.findall('[0-9]+', s)[0])
     
 
 class uabXvalParent(object):
@@ -34,5 +85,38 @@ class uabXvalParent(object):
         #one index per row in the list of lists of chips or tiles by their channel
         raise NotImplementedError('Must be implemented by the subclass')
 
+
 class uabXvalByCity(uabXvalParent):
     def computeFolds(self, chipFiles):
+        idx = []
+        cnt = 0
+        city_set = {}
+        for row in chipFiles:
+            city_name = getCityName(row)
+            if city_name in city_set:
+                # get idx for this city
+                idx.append(city_set[city_name])
+            else:
+                # a new city, update dict and get idx
+                city_set[city_name] = cnt
+                cnt += 1
+                idx.append(city_set[city_name])
+        return idx
+
+
+class uabXvalByTile(uabXvalParent):
+    def computeFolds(self, chipFiles):
+        idx = []
+        cnt = 0
+        tile_set = {}
+        for row in chipFiles:
+            tile_number = getTileNumber(row)
+            if tile_number in tile_set:
+                # get idx for this city
+                idx.append(tile_set[tile_number])
+            else:
+                # a new city, update dict and get idx
+                tile_set[tile_number] = cnt
+                cnt += 1
+                idx.append(tile_set[tile_number])
+        return idx
