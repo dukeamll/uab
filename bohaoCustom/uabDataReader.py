ce6442bcbb2c123ffb83c06b8556a8fcca9b9720
bohaohuang <bohao.huang@duke.edu>
2017-12-22 10:38:17 -0500

    xcity data reader

--- bohaoCustom/uabDataReader.py
+++ bohaoCustom/uabDataReader.py
@@ -23,6 +23,25 @@ def group_by_tile_name(tile_name, chip_files):
     return group
 
 
+def group_by_city_name(tile_name, chip_files):
+    '''group = [[] for i in range(len(tile_name))]
+    tile_dict = {}
+    for cnt, name in enumerate(tile_name):
+        tile_dict[name[:3]] = cnt
+    print(tile_dict)
+    for item in chip_files:
+        group[tile_dict[item[0].split('_')[0]][:3]].append(item)
+    return group'''
+    cities = list(set([a[:3] for a in tile_name]))
+    city_dict = {}
+    for cnt, name in enumerate(cities):
+        city_dict[name] = cnt
+    group = [[] for i in range(len(cities))]
+    for item in chip_files:
+        group[city_dict[item[0][:3]]].append(item)
+    return group
+
+
 # class to load all the possible slices of your data
 class ImageLabelReader(object):
     def __init__(self, gtInds, dataInds, parentDir, chipFiles, chip_size, batchSize, nChannels=1,
@@ -153,45 +172,41 @@ class ImageLabelReader(object):
         else:
             # random, batches has to from different tiles
             tile_num, patch_per_tile, tile_name = get_tile_and_patch_num(chipFiles)
-            group = group_by_tile_name(tile_name, chipFiles)
+            group = group_by_city_name(tile_name, chipFiles)
 
-            '''tile_idx = np.random.permutation(tile_num)
-            patch_idx = np.random.permutation(patch_per_tile)
-            if patch_per_tile % batch_size != 0:
-                comp_len = batch_size - patch_per_tile % batch_size
-                patch_idx = np.append(patch_idx, patch_idx[:comp_len])
+            tile_idx = np.random.permutation(len(group))
             nDims = len(chipFiles[0])
             while True:
                 image_batch = np.zeros((batch_size, patch_size[0], patch_size[1], nDims))
-                for randInd in tile_idx:
-                    for cnt, patchInd in enumerate(patch_idx):
-                        row = group[randInd][patchInd]
-                        blockList = []
-                        nDims = 0
-                        for file in row:
-                            img = util_functions.uabUtilAllTypeLoad(os.path.join(image_dir, file))
-                            if len(img.shape) == 2:
-                                img = np.expand_dims(img, axis=2)
-                            nDims += img.shape[2]
-                            blockList.append(img)
-                        block = np.dstack(blockList)
-                        block = block.astype(np.float32)
+                for cnt, randInd in enumerate(tile_idx):
+                    patchInd = np.random.randint(low=0, high=len(group[0]))
+                    row = group[randInd][patchInd]
+                    blockList = []
+                    nDims = 0
+                    for file in row:
+                        img = util_functions.uabUtilAllTypeLoad(os.path.join(image_dir, file))
+                        if len(img.shape) == 2:
+                            img = np.expand_dims(img, axis=2)
+                        nDims += img.shape[2]
+                        blockList.append(img)
+                    block = np.dstack(blockList)
+                    block = block.astype(np.float32)
 
-                        if self.block_mean is not None:
-                            block -= self.block_mean
+                    if self.block_mean is not None:
+                        block -= self.block_mean
 
-                        if dataAug != '':
-                            augDat = uabUtilreader.doDataAug(block, nDims, dataAug, is_np=True)
-                        else:
-                            augDat = block
+                    if dataAug != '':
+                        augDat = uabUtilreader.doDataAug(block, nDims, dataAug, is_np=True)
+                    else:
+                        augDat = block
 
-                        if (np.array(padding) > 0).any():
-                            augDat = uabUtilreader.pad_block(augDat, padding)
+                    if (np.array(padding) > 0).any():
+                        augDat = uabUtilreader.pad_block(augDat, padding)
 
-                        image_batch[cnt % batch_size, :, :, :] = augDat
+                    image_batch[cnt % batch_size, :, :, :] = augDat
 
-                        if ((cnt + 1) % batch_size == 0):
-                            yield image_batch[:, :, :, 1:], image_batch[:, :, :, :1]'''
+                    if ((cnt + 1) % batch_size == 0):
+                        yield image_batch[:, :, :, 1:], image_batch[:, :, :, :1]
 
 
 # for debugging purposes
@@ -227,13 +242,14 @@ if __name__ == '__main__':
 
     dataReader_train = ImageLabelReader([3], [0, 1, 2], patchDir, file_list_train, (572, 572),
                                         5, dataAug='flip,rotate', block_mean=np.append([0], img_mean),
-                                        batch_code=1)
-    x, y = dataReader_train.readerAction()
+                                        batch_code=2)
 
-    import matplotlib.pyplot as plt
-    for i in range(5):
-        plt.subplot(5, 2, i*2+1)
-        plt.imshow(x[i, :, :, :]+img_mean)
-        plt.subplot(5, 2, i*2+1+1)
-        plt.imshow(y[i, :, :, 0])
-    plt.show()
+    for plt_cnt in range(10):
+        x, y = dataReader_train.readerAction()
+        import matplotlib.pyplot as plt
+        for i in range(5):
+            plt.subplot(5, 2, i*2+1)
+            plt.imshow(x[i, :, :, :]+img_mean)
+            plt.subplot(5, 2, i*2+1+1)
+            plt.imshow(y[i, :, :, 0])
+        plt.show()
