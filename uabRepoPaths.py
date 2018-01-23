737e3f15158e2c55245e8742b7f6e4a022dc6185
bohaohuang <bohao.huang@duke.edu>
2018-01-23 13:56:17 -0500

    add uabRepoPaths.py

---
+++ uabRepoPaths.py
@@ -0,0 +1,15 @@
+"""
+Created on Wed Dec  6 15:22:01 2017
+Modified on Tues Jan 23 2018 by Bohao
+
+@author: Daniel
+
+This file contains the paths for the results and the data.
+Must have a dataPath and resPath defined
+"""
+
+parentDir = r'/media/ei-edl01/data/uab_datasets'
+dataPath = parentDir
+resPath = r'/hdd/uab_datasets/Results'
+modelPath = r'/hdd/Models'
+evalPath = r'/hdd/Results'
