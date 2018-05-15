39c3fa541d53cfde21b54d023a2386f3f22f9bfc
bohaohuang <bohao.huang@duke.edu>
2018-05-15 15:59:45 -0400

    bigan version alpha

--- bohaoCustom/uabMakeNetwork_BiGAN.py
+++ bohaoCustom/uabMakeNetwork_BiGAN.py
@@ -97,7 +97,7 @@ class BiGAN(uabMakeNetwork_DCGAN.DCGAN):
                  batch_size=5, start_filter_num=32, z_dim=1000, lr_mult=5, beta1=0.5):
         network.Network.__init__(self, inputs, trainable, dropout_rate,
                                  learn_rate, decay_step, decay_rate, epochs, batch_size)
-        self.name = 'DCGAN'
+        self.name = 'BiGAN'
         self.model_name = self.get_unique_name(model_name)
         self.sfn = start_filter_num
         self.learning_rate = None
@@ -130,6 +130,15 @@ class BiGAN(uabMakeNetwork_DCGAN.DCGAN):
         self.D_, self.D_logits_ = [], []
         self.d_loss, self.g_loss = [], []
 
+    def encoder(self, input_):
+        with tf.variable_scope('encoder'):
+            h = lrelu(conv2d(input_, self.sfn, name='e_h0_conv'))
+            for i in range(self.depth - 1):
+                h = lrelu(self.e_bn[i](conv2d(h, self.sfn * 2 ** (i + 1), name='e_h{}_conv'.format(i + 1))))
+            h = linear(tf.reshape(h, [self.bs, 4 * 4 * self.sfn * 2 ** (self.depth - 1)]), self.z_dim,
+                       'e_h{}_lin'.format(self.depth + 1))
+            return tf.nn.tanh(h)
+
     def generator(self, z):
         with tf.variable_scope('generator'):
             # calculate height & width at each layer
@@ -152,15 +161,6 @@ class BiGAN(uabMakeNetwork_DCGAN.DCGAN):
                                    with_w=True)
 
             return tf.nn.tanh(h), z
-
-    def encoder(self, input_):
-        with tf.variable_scope('encoder'):
-            h = lrelu(conv2d(input_, self.sfn, name='e_h0_conv'))
-            for i in range(self.depth - 1):
-                h = lrelu(self.e_bn[i](conv2d(h, self.sfn * 2 ** (i + 1), name='e_h{}_conv'.format(i + 1))))
-            h = linear(tf.reshape(h, [self.bs, 4 * 4 * self.sfn * 2 ** (self.depth - 1)]), self.z_dim,
-                       'e_h{}_lin'.format(self.depth + 1))
-            return tf.nn.tanh(h)
 
     def discriminator(self, input_, encoded, minibatch_dis=True, reuse=False, n_kernels=300, dim_per_kernel=50):
         with tf.variable_scope('discriminator') as scope:
@@ -240,152 +240,10 @@ class BiGAN(uabMakeNetwork_DCGAN.DCGAN):
 
     def make_optimizer(self, train_var_filter):
         t_vars = tf.trainable_variables()
-        d_vars = [var for var in t_vars if 'd_' in var.name] + [var for var in t_vars if 'e_' in var.name]
-        g_vars = [var for var in t_vars if 'g_' in var.name]
+        d_vars = [var for var in t_vars if 'd_' in var.name]
+        g_vars = [var for var in t_vars if 'g_' in var.name] + [var for var in t_vars if 'e_' in var.name]
         optm_d = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).\
             minimize(self.d_loss, var_list=d_vars, global_step=self.global_step)
         optm_g = tf.train.AdamOptimizer(self.learning_rate * self.lr_mult, beta1=self.beta1).\
             minimize(self.g_loss, var_list=g_vars, global_step=self.global_step)
         self.optimizer = {'d': optm_d, 'g': optm_g}
-
-    def make_update_ops(self, x_name, z_name):
-        tf.add_to_collection('inputs', self.inputs[x_name])
-        tf.add_to_collection('inputs', self.inputs[z_name])
-        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-
-    def make_summary(self, hist=False):
-        tf.summary.scalar('d loss', self.d_loss)
-        tf.summary.scalar('g loss', self.g_loss)
-        tf.summary.scalar('learning rate', self.learning_rate)
-        self.summary = tf.summary.merge_all()
-
-    def train_config(self, x_name, z_name, n_train, n_valid, patch_size, ckdir, loss_type='xent', train_var_filter=None,
-                     hist=False, par_dir=None, **kwargs):
-        self.make_loss(z_name, loss_type, **kwargs)
-        self.make_learning_rate(n_train * 3)
-        self.make_update_ops(x_name, z_name)
-        self.make_optimizer(train_var_filter)
-        self.make_ckdir(ckdir, patch_size, par_dir)
-        self.make_summary()
-        self.config = tf.ConfigProto()
-        self.n_train = n_train
-        self.n_valid = n_valid
-
-    def train(self, x_name, z_name, n_train, sess, summary_writer, n_valid=1000,
-              train_reader=None, valid_reader=None,
-              image_summary=None, verb_step=100, save_epoch=5,
-              img_mean=np.array((0, 0, 0), dtype=np.float32),
-              continue_dir=None, valid_iou=False):
-        # define summary operations
-        valid_g_summary_op = tf.summary.scalar('g_loss_validation', self.valid_g_summary)
-        valid_d_summary_op = tf.summary.scalar('d_loss_validation', self.valid_d_summary)
-        valid_image_summary_op = tf.summary.image('Validation_images_summary', self.valid_images,
-                                                  max_outputs=10)
-
-        if continue_dir is not None:
-            self.load(continue_dir, sess)
-            gs = sess.run(self.global_step)
-            start_epoch = int(np.ceil(gs/n_train*self.bs))
-            start_step = gs - int(start_epoch*n_train/self.bs)
-        else:
-            start_epoch = 0
-            start_step = 0
-
-        loss_valid_min = np.inf
-        sampler = stats.truncnorm(-1, 1, loc=0, scale=1)
-        for epoch in range(start_epoch, self.epochs):
-            start_time = time.time()
-            for step_cnt, step in enumerate(range(start_step, n_train, self.bs)):
-                X_batch, _ = train_reader.readerAction(sess)
-                Z_batch = np.random.uniform(-1, 1, [self.bs, self.z_dim]).astype(np.float32)
-                X_batch = 2 * (X_batch / 255 - 0.5)
-                _, _, self.global_step_value = sess.run([self.optimizer['d'], self.optimizer['g'], self.global_step],
-                                                        feed_dict={self.inputs[x_name]: X_batch,
-                                                                   self.inputs[z_name]: Z_batch,
-                                                                   self.trainable: True})
-                Z_batch = np.random.uniform(-1, 1, [self.bs, self.z_dim]).astype(np.float32)
-                _ = sess.run(self.optimizer['g'], feed_dict={self.inputs[x_name]: X_batch,
-                                                             self.inputs[z_name]: Z_batch,
-                                                             self.trainable: True})
-
-                if step_cnt % verb_step == 0:
-                    d_loss, g_loss, step_summary = sess.run([self.d_loss, self.g_loss, self.summary],
-                                                            feed_dict={self.inputs[x_name]: X_batch,
-                                                                       self.inputs[z_name]: Z_batch,
-                                                                       self.trainable: False})
-                    summary_writer.add_summary(step_summary, self.global_step_value)
-                    print('Epoch {:d} step {:d}\td_loss = {:.3f}, g_loss = {:.3f}'.
-                          format(epoch, step_cnt, d_loss, g_loss))
-            # validation
-            loss_valid_mean = []
-            g_loss_val_mean = []
-            d_loss_val_mean = []
-            for step in range(0, n_valid, self.bs):
-                X_batch_val, _ = valid_reader.readerAction(sess)
-                Z_batch_val = np.random.uniform(-1, 1, [self.bs, self.z_dim]).astype(np.float32)
-                X_batch_val = 2 * (X_batch_val / 255 - 0.5)
-                d_loss_val, g_loss_val = sess.run([self.d_loss, self.g_loss],
-                                                  feed_dict={self.inputs[x_name]: X_batch_val,
-                                                             self.inputs[z_name]: Z_batch_val,
-                                                             self.trainable: False})
-                loss_valid_mean.append(d_loss_val+g_loss_val)
-                g_loss_val_mean.append(g_loss_val)
-                d_loss_val_mean.append(d_loss_val)
-            loss_valid_mean = np.mean(loss_valid_mean)
-            duration = time.time() - start_time
-            print('Validation loss: {:.3f}, duration: {:.3f}'.format(loss_valid_mean, duration))
-            valid_g_summary = sess.run(valid_g_summary_op,
-                                       feed_dict={self.valid_g_summary: np.mean(g_loss_val_mean)})
-            valid_d_summary = sess.run(valid_d_summary_op,
-                                       feed_dict={self.valid_d_summary: np.mean(d_loss_val_mean)})
-            summary_writer.add_summary(valid_g_summary, self.global_step_value)
-            summary_writer.add_summary(valid_d_summary, self.global_step_value)
-            if loss_valid_mean < loss_valid_min:
-                loss_valid_min = loss_valid_mean
-                saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=1)
-                saver.save(sess, '{}/best_model.ckpt'.format(self.ckdir))
-
-            Z_batch_sample = np.random.uniform(-1, 1, [self.bs, self.z_dim]).astype(np.float32)
-            valid_img_gen = sess.run(self.G, feed_dict={self.inputs[z_name]: Z_batch_sample})
-            if image_summary is not None:
-                valid_image_summary = sess.run(valid_image_summary_op,
-                                               feed_dict={self.valid_images:
-                                                              image_summary(valid_img_gen)})
-                summary_writer.add_summary(valid_image_summary, self.global_step_value)
-
-            if epoch % save_epoch == 0:
-                saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=1)
-                saver.save(sess, '{}/model_{}.ckpt'.format(self.ckdir, epoch), global_step=self.global_step)
-
-    def run(self, train_reader=None, valid_reader=None, test_reader=None, pretrained_model_dir=None, layers2load=None,
-            isTrain=False, img_mean=np.array((0, 0, 0), dtype=np.float32), verb_step=100, save_epoch=5, gpu=None,
-            tile_size=(5000, 5000), patch_size=(572, 572), truth_val=1, continue_dir=None, load_epoch_num=None,
-            fineTune=False, valid_iou=False, best_model=True):
-        if gpu is not None:
-            os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-            os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
-        if isTrain:
-            coord = tf.train.Coordinator()
-            with tf.Session(config=self.config) as sess:
-                # init model
-                init = [tf.global_variables_initializer(), tf.local_variables_initializer()]
-                sess.run(init)
-                saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=1)
-                # load model
-                if pretrained_model_dir is not None:
-                    if layers2load is not None:
-                        self.load_weights(pretrained_model_dir, layers2load)
-                    else:
-                        self.load(pretrained_model_dir, sess, saver, epoch=load_epoch_num)
-                threads = tf.train.start_queue_runners(coord=coord, sess=sess)
-                try:
-                    train_summary_writer = tf.summary.FileWriter(self.ckdir, sess.graph)
-                    self.train('X', 'Z', self.n_train, sess, train_summary_writer,
-                               n_valid=self.n_valid, train_reader=train_reader, valid_reader=valid_reader,
-                               image_summary=image_summary, img_mean=img_mean,
-                               verb_step=verb_step, save_epoch=save_epoch, continue_dir=continue_dir,
-                               valid_iou=valid_iou)
-                finally:
-                    coord.request_stop()
-                    coord.join(threads)
-                    saver.save(sess, '{}/model.ckpt'.format(self.ckdir), global_step=self.global_step)
