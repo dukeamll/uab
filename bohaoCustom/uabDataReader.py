a78ab7d5743d7612fef87e58d186e56cbf1ca617
bohaohuang <bohao.huang@duke.edu>
2018-06-22 23:31:18 -0400

    loo mtl

--- bohaoCustom/uabDataReader.py
+++ bohaoCustom/uabDataReader.py
@@ -478,6 +478,61 @@ class ImageLabelReaderPatchSampleControl(ImageLabelReader):
                     else:
                         yield image_batch[:, :, :, 1:], image_batch[:, :, :, :1]
 
+class ImageLabelReaderBuilding(ImageLabelReader):
+    def __init__(self, gtInds, dataInds, parentDir, chipFiles, chip_size, batchSize, patch_prob,
+                 nChannels=1, padding=np.array((0, 0)), block_mean=None, dataAug=''):
+        self.patch_prob = patch_prob
+        super(ImageLabelReaderBuilding, self).__init__(gtInds, dataInds, parentDir,
+                                                       chipFiles,
+                                                       chip_size,
+                                                       batchSize, nChannels, padding,
+                                                       block_mean, dataAug)
+
+    def readFromDiskIteratorTrain(self, image_dir, chipFiles, batch_size, patch_size,
+                                  padding=(0, 0), dataAug=''):
+        # this is a iterator for training
+        nDims = len(chipFiles[0])
+        while True:
+            image_batch = np.zeros((batch_size, patch_size[0], patch_size[1], nDims))
+            building_truth = np.zeros((batch_size, 1))
+            # select number to sample
+            idx_batch = np.random.permutation(len(chipFiles))
+            for cnt, randInd in enumerate(idx_batch):
+                row = chipFiles[randInd]
+                p_name = '_'.join(row[0].split('_')[:2])
+
+                blockList = []
+                nDims = 0
+                for file in row:
+                    img = util_functions.uabUtilAllTypeLoad(os.path.join(image_dir, file))
+                    if len(img.shape) == 2:
+                        img = np.expand_dims(img, axis=2)
+                    nDims += img.shape[2]
+                    blockList.append(img)
+                block = np.dstack(blockList).astype(np.float32)
+
+                if self.block_mean is not None:
+                    block -= self.block_mean
+
+                if dataAug != '':
+                    augDat = uabUtilreader.doDataAug(block, nDims, dataAug, is_np=True,
+                                                     img_mean=self.block_mean)
+                else:
+                    augDat = block
+
+                if (np.array(padding) > 0).any():
+                    augDat = uabUtilreader.pad_block(augDat, padding)
+
+                store_idx = cnt % batch_size
+                image_batch[store_idx, :, :, :] = augDat
+                if np.sum(image_batch[store_idx, :, :, 0]) / (patch_size[0] * patch_size[0]) > self.patch_prob:
+                    building_truth[store_idx, :] = 1
+                else:
+                    building_truth[store_idx, :] = 0
+
+                if (cnt + 1) % batch_size == 0:
+                    yield image_batch[:, :, :, 1:], image_batch[:, :, :, :1], building_truth
+
 
 # for debugging purposes
 if __name__ == '__main__':
@@ -485,8 +540,6 @@ if __name__ == '__main__':
     import bohaoCustom.uabPreprocClasses as bPreproc
     import uab_DataHandlerFunctions
     import uabCrossValMaker
-
-    patch_prob = np.load('/media/ei-edl01/user/bh163/tasks/2018.06.01.domain_selection/patch_prob_austin.npy')
 
     # create collection
     # the original file is in /ei-edl01/data/uab_datasets/inria
@@ -512,23 +565,19 @@ if __name__ == '__main__':
     file_list_train = uabCrossValMaker.make_file_list_by_key(idx, file_list, [i for i in range(6, 37)])
     file_list_valid = uabCrossValMaker.make_file_list_by_key(idx, file_list, [i for i in range(0, 6)])
 
-    dataReader_train = ImageLabelReaderPatchSampleControl([3], [0, 1, 2], patchDir, file_list_train, (321, 321),
-                                                          5, patch_prob, patch_name=True,
-                                                          block_mean=np.append([0], img_mean))
-    _, _, patch_name = dataReader_train.readerAction()
-    print(patch_name)
-    _, _, patch_name = dataReader_train.readerAction()
-    print(patch_name)
+    dataReader_train = ImageLabelReaderBuilding([3], [0, 1, 2], patchDir, file_list_train, (321, 321),
+                                                5, 0.1, block_mean=np.append([0], img_mean))
 
-    '''for plt_cnt in range(10):
-        x, y = dataReader_train.readerAction()
+    for plt_cnt in range(10):
+        x, y, b = dataReader_train.readerAction()
+        print(b)
         import matplotlib.pyplot as plt
         for i in range(5):
             plt.subplot(5, 2, i*2+1)
             plt.imshow((x[i, :, :, :]+img_mean).astype(np.uint8))
             plt.subplot(5, 2, i*2+1+1)
             plt.imshow(y[i, :, :, 0])
-        plt.show()'''
+        plt.show()
 
     '''idx_all = np.zeros(50000)
     for plt_cnt in range(10000):
